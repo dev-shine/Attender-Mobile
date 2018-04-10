8d80dd6408f05a9bdaa19e91a26fe966bdeacd1f
robbieve <robbieeve751994@gmail.com>
2018-04-10 23:55:13 +0800

    Redo AppConfig.js

--- app/Configs/AppConfig.js
+++ app/Configs/AppConfig.js
@@ -3,7 +3,7 @@
  */
 
 var config = {
-  API_URL: 'http://10.0.2.2:3333/api/',
+  API_URL: 'https://staging.attender.com.au/api/',
   CLOUDINARY_KEY: '535433155736131',
   CLOUDINARY_SECRET: 'HgpOaFgqpc30FCYI9fzbbo858BI',
   CLOUDINARY_UPLOAD_URL: 'https://api.cloudinary.com/v1_1/drkmr71us/'
